239da660c65771ddae874e5a0957a0303f716181
Can Ur <canur11@gmail.com>
2022-02-13 11:56:08 +0100

    RayFire + Feel

---
+++ RayFireProject/Assets/RayFire/Scripts/Classes/Rigid/RFFragmentProperties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RayFire
+{
+	[Serializable]
+	public class RFFragmentProperties
+	{
+		public RFColliderType colliderType;
+		public float sizeFilter;
+		public bool decompose;
+		public bool removeCollinear;
+		public string layer;
+
+		/// /////////////////////////////////////////////////////////
+        /// Constructor
+        /// /////////////////////////////////////////////////////////
+		
+		// Constructor
+		public RFFragmentProperties()
+		{
+			colliderType = RFColliderType.Mesh;
+			sizeFilter   = 0;
+			decompose    = false;
+
+			removeCollinear = false;
+			layer           = "";
+		}
+
+		// Copy from
+		public void CopyFrom (RFFragmentProperties fragmentProperties)
+		{
+			colliderType = fragmentProperties.colliderType;
+			sizeFilter   = fragmentProperties.sizeFilter;
+			decompose    = false;
+
+			removeCollinear = fragmentProperties.removeCollinear;
+			layer           = fragmentProperties.layer;
+		}
+	}
+}
---
+++ RayFireProject/Assets/RayFire/Tutorial/Scripts/ConnectivityEventScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+// IMPORTANT! You should use RayFire namespace to use RayFire component's event.
+using RayFire;
+
+// Tutorial script. Allows to subscribe to Connectivity component activation.
+public class ConnectivityEventScript : MonoBehaviour
+{
+    // Define if script should subscribe to global Connectivity activation event
+    public bool globalSubscription = false;
+    
+    // Local Connectivity component which will be checked for activation.
+    // You can get RayfireConnectivity component which you want to check for activation in any way you want.
+    // This is just a tutorial way to define it.
+    public bool                localSubscription = false;
+    public RayfireConnectivity  localConnectivityComponent;
+    
+    // /////////////////////////////////////////////////////////
+    // Subscribe/Unsubscribe
+    // /////////////////////////////////////////////////////////
+    
+    // Subscribe to event
+    void OnEnable()
+    {
+        // Subscribe to global Connectivity activation event. Every activation will invoke subscribed methods. 
+        if (globalSubscription == true)
+            RFConnectivityEvent.GlobalEvent += GlobalMethodConnectivity;
+        
+        // Subscribe to local activation event. Activation in specific Connectivity component will invoke subscribed methods. 
+        if (localSubscription == true && localConnectivityComponent != null)
+            localConnectivityComponent.connectivityEvent.LocalEvent += LocalMethodConnectivity;
+    }
+    
+    // Unsubscribe from event
+    void OnDisable()
+    {
+        // Unsubscribe from global Connectivity activation event.
+        if (globalSubscription == true)
+            RFConnectivityEvent.GlobalEvent -= GlobalMethodConnectivity;
+        
+        // Unsubscribe from local Connectivity activation event.
+        if (localSubscription == true && localConnectivityComponent != null)
+            localConnectivityComponent.connectivityEvent.LocalEvent -= LocalMethodConnectivity;
+    }
+
+    // /////////////////////////////////////////////////////////
+    // Subscription Methods
+    // /////////////////////////////////////////////////////////
+    
+    // IMPORTANT!. Subscribed method should has following signature.
+    // Void return type and one RayfireConnectivity input parameter.
+    // RayfireConnectivity input parameter is Connectivity component which was activated.
+   
+    // Method for local activation subscription
+    void LocalMethodConnectivity(RayfireConnectivity connectivity)
+    {
+        Debug.Log("Local Connectivity activation: " + connectivity.name + " has " + connectivity.AmountIntegrity + " % Integrity");
+    }
+    
+    // Method for global activation subscription
+    void GlobalMethodConnectivity(RayfireConnectivity connectivity)
+    {
+        Debug.Log("Global Connectivity activation: "+ connectivity.name + " has " + connectivity.AmountIntegrity + " % Integrity");
+    }
+}
